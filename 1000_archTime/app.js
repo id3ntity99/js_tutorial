8798330673e0831f69a7aa91448fcb62fa66ab6a
id3ntity00 <jas06113@gmail.com>
2021-07-19 19:24:31 +0900

    Update app.js

--- 1000_archTime/app.js
+++ 1000_archTime/app.js
@@ -1,5 +1,8 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
-const URL = axios.get("http://google.co.kr");
-
-console.log(URL);
+axios.get("https://kr.indeed.com/jobs?q=javascript&l=").then((res) => {
+  const $ = cheerio.load(res.data);
+  const title = $(".jobTitle span").text();
+  console.log(title);
+});
