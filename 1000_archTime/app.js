efbe49bbd99c0c22006e5ee6313a9126a0738a1b
id3ntity00 <jas06113@gmail.com>
2021-07-20 23:08:49 +0900

    web_scraper
    Adding Web Scraping script

--- 1000_archTime/app.js
+++ 1000_archTime/app.js
@@ -1,8 +1,28 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-axios.get("https://kr.indeed.com/jobs?q=javascript&l=").then((res) => {
+const URL = "http://www.archiworldjournal.com/news/articleList.html";
+const LINKS = [];
+
+axios.get(URL).then((res) => {
   const $ = cheerio.load(res.data);
-  const title = $(".jobTitle span").text();
-  console.log(title);
+  //=======Get links from each articles.======
+  const getLinks = $(".links");
+  getLinks.each((idx, val) => {
+    const link = $(val).attr("href");
+    if (link === "") {
+      return;
+    }
+    //Delete empty str
+    const newURL = "http://www.archiworldjournal.com" + `${link}`;
+    LINKS.push({ "Article link": newURL });
+  });
+  //=======Get links from each articles.======
+  console.log(LINKS);
 });
+
+/*  const title = $(".links").text();
+  //Get titles of each articels
+
+  const date = $(".list-dated").text();
+  //Get date and name of reporter */
